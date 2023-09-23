4e64f73a39ca29ccba50ba7b2ab411e2be5cb14c
shiva-1612 <115456476+shiva-1612@users.noreply.github.com>
2023-09-23 16:44:43 +0530

    1 file modified

--- building_data.js
+++ building_data.js
@@ -87,7 +87,7 @@ const MeetingRooms = [
 const Meeting = [
   {
     id : 1,
-    title : 'Quick Books daily stand up',
+    title : 'Daily Booking',
     date : '25/09/2023',
     startTime : "9:00 AM",
     endTime : "6:00 PM",
